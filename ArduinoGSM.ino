aefd557560f1fc3cefdc0e198ae320f6d2a40c35
galiper34 <galiper34@gmail.com>
2016-02-27 22:26:41 +0100

    Update ArduinoGSM.ino

--- ArduinoGSM.ino
+++ ArduinoGSM.ino
@@ -10,42 +10,25 @@
 //Create software serial object to communicate with SIM800
 SoftwareSerial serialSIM800(SIM800_TX_PIN,SIM800_RX_PIN);
 
-//Code PIN
-String SIM_PIN_CODE = String("xxxx");
 //conditionnel d'envoi de sms
 boolean sms;
- 
-void setup() {
+//--------------------------------------------------------------------------------
+ void setup() {
   //Begin serial comunication with Arduino and Arduino IDE (Serial Monitor)
   Serial.begin(9600);
-  while(!Serial);
+  //while(!Serial);
    
   //Being serial communication witj Arduino and SIM800
-  serialSIM800.begin(9600);
+  serialSIM800.begin(19200);
   
   delay(1000);
-  //Initialisation
-  Serial.println("---## Initialisation ##---");
-  Serial.println("----- Code PIN -----");
-  serialSIM800.print("AT+CPIN=");
-  serialSIM800.println(SIM_PIN_CODE);
-  delay(5000);
-  Serial.println("----- Statut -----");
-  serialSIM800.println("ATI"); 
-  delay(5000);
-  Serial.println("----- Etat du Reseau -----");
-  serialSIM800.println("AT+CSQ"); 
-  delay(5000);
-  Serial.println("----- Operateur -----");
-  serialSIM800.println("AT+COPS ?"); 
-  delay(5000);   
-  Serial.println("---## Setup Complete ##---");
-  Serial.println("---## SendSMS ##---");
+  InitialisationGSM(serialSIM800,"0000");
+  Serial.println("---## Send TEST SMS ##---");
   
   //envoi de sms de test
   sms = false;
 }
- 
+//-------------------------------------------------------------------------------- 
 void loop() {
   //Read SIM800 output (if available) and print it in Arduino IDE Serial Monitor
   if(serialSIM800.available()){
@@ -54,31 +37,52 @@ void loop() {
   //Read Arduino IDE Serial Monitor inputs (if available) and send them to SIM800
   if(Serial.available()){    
     serialSIM800.write(Serial.read());
+    serialSIM800.println();
   }
 
   if(sms==true){
-    sendsms("Message ici","33600000000");        
+    sendSms(serialSIM800,"Msg de test !!","33600000000");        
     sms = false;
   }
 }
-
-  void sendsms(String messageEnvoi, String numeroTel) {
-    Serial.println("Début d'envoi de message");
-    serialSIM800.print("AT+CMGF=1\r");  // Lance le mode SMS
+//--------------------------------------------------------------------------------
+void InitialisationGSM(SoftwareSerial serialGSM, String pinGSM) {
+  Serial.println("---## Initialisation ##---");
+  Serial.println("----- Code PIN -----");
+  serialGSM.print("AT+CPIN=");
+  serialGSM.println(pinGSM);
+  delay(5000);
+  Serial.println("----- Statut -----");
+  serialGSM.println("ATI"); 
+  delay(4000);
+  Serial.println("----- Etat du Reseau -----");
+  serialGSM.println("AT+CSQ"); 
+  delay(4000);
+  Serial.println("----- Operateur -----");
+  serialGSM.println("AT+COPS ?"); 
+  delay(4000);   
+}
+//--------------------------------------------------------------------------------    
+void sendSms(SoftwareSerial serialGSM, String messageEnvoi, String numeroTel) {
+  Serial.println("Début d'envoi de message");
+    serialGSM.print("AT+CMGF=1\r");  // Lance le mode SMS
     delay(1000);
     // Entrez votre numéro de téléphone    
-    serialSIM800.print("AT+CMGS=\"+");
-    serialSIM800.print(numeroTel);
-    serialSIM800.print("\"\r");  
+    serialGSM.print("AT+CMGS=\"+");
+    serialGSM.print(numeroTel);
+    serialGSM.print("\"\r");  
     delay(1000);
     // Entrez votre message ici    
-    serialSIM800.print(messageEnvoi);
-    serialSIM800.print(" \r");  
+    serialGSM.print(messageEnvoi);
+    serialGSM.print(" \r");  
     // CTR+Z en langage ASCII, indique la fin du message
-    serialSIM800.print(char(26));        
+    serialGSM.print(char(26));        
     delay(1000);
-    serialSIM800.println();
-    Serial.print("SMS pour ");
+    serialGSM.println();
+    Serial.print("SMS : ");
+    Serial.print(messageEnvoi);
+    Serial.print(" Pour :");
     Serial.print(numeroTel);
     Serial.println(" Envoyé");
-    }
+}
+//--------------------------------------------------------------------------------
